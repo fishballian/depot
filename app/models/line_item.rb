3307702a2cbc9e8666f4389c7bf0c2913205c632
fishballian <fishballian@qq.com>
2013-07-11 13:36:09 +0800

    add orders

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,10 @@
 class LineItem < ActiveRecord::Base
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
   attr_accessible :cart_id, :product_id
   attr_accessible :product
+  def total_price
+    product.price * quantity
+  end
 end
